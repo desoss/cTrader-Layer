fde3a3723120e25d8d9d2ca29f71ef83a700b070
Vasile Pește <vasile.peste@protonmail.ch>
2022-01-24 21:38:58 +0100

    Add environemnt check

--- src/core/utilities/CTraderLayerUtilities.ts
+++ src/core/utilities/CTraderLayerUtilities.ts
@@ -2,4 +2,8 @@ export class CTraderLayerUtilities {
     private constructor () {
         // Silence is golden.
     }
+
+    public static isBrowser (): boolean {
+        return !(typeof "process" !== "undefined" && process && process.versions && process.versions.node);
+    }
 }
